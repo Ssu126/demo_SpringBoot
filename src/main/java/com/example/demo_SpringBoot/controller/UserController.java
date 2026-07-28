d9da8cfef3f335a6fff51fa661aabd121ea7f0f8
Suyoung <sueyoungkim69@gmail.com>
2026-07-28 18:50:17 +0900

    실습 5-10

--- src/main/java/com/example/demo_SpringBoot/controller/UserController.java
+++ src/main/java/com/example/demo_SpringBoot/controller/UserController.java
@@ -3,15 +3,25 @@ package com.example.demo_SpringBoot.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class UserController {
     @RequestMapping(method = RequestMethod.GET, value = "/users")
-    public String userPage() {
-        return "/users/list";
+    public ModelAndView userPage() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("/user/list");
+        return modelAndView;
     }
     @RequestMapping(method = RequestMethod.GET, value = "/users/1/detail")
-    public String detailPage() {
-        return "/users/detail";
+    public ModelAndView detailPage() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("id", 1);
+        modelAndView.addObject("name", "Aaron");
+        modelAndView.addObject("age", 10);
+        modelAndView.addObject("job", "Developer");
+        modelAndView.addObject("speciality", "Backend");
+        modelAndView.setViewName("/users/detail");
+        return modelAndView;
     }
 }
