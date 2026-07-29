ade275d6e33ef3b6e9c970239f4edd01ad3107f6
Suyoung <sueyoungkim69@gmail.com>
2026-07-29 15:14:01 +0900

    실습 5-13

--- src/main/java/com/example/demo_SpringBoot/controller/UserController.java
+++ src/main/java/com/example/demo_SpringBoot/controller/UserController.java
@@ -1,28 +1,40 @@
 package com.example.demo_SpringBoot.controller;
 
+import com.example.demo_SpringBoot.service.User;
+import com.example.demo_SpringBoot.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/users")
 public class UserController {
+    @Autowired
+    private UserService userService;
+
     @GetMapping("")
     public ModelAndView userPage() {
         ModelAndView modelAndView = new ModelAndView();
+        List<User> users = userService.findAll();
+        modelAndView.addObject("users", users);
         modelAndView.setViewName("/user/list");
         return modelAndView;
     }
+
     @GetMapping("/1/detail")
     public ModelAndView detailPage() {
         ModelAndView modelAndView = new ModelAndView();
-        modelAndView.addObject("id", 1);
-        modelAndView.addObject("name", "Aaron");
-        modelAndView.addObject("age", 10);
-        modelAndView.addObject("job", "Developer");
-        modelAndView.addObject("speciality", "Backend");
+        User user = userService.findById(1);
+        modelAndView.addObject("id", user.getId());
+        modelAndView.addObject("name", user.getName());
+        modelAndView.addObject("age", user.getAge());
+        modelAndView.addObject("job", user.getJob());
+        modelAndView.addObject("speciality", user.getSpecialty());
         modelAndView.setViewName("/users/detail");
         return modelAndView;
     }
